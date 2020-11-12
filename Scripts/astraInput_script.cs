9d602ba65e65524cc45b45537eed3f8848c5d33a
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-11-11 18:11:58 -0700

    Add files via upload

---
+++ Scripts/astraInput_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astraInput_script : MonoBehaviour
+{
+    Animator astraAnimator;
+    float verticalPress;
+    void Start()
+    {
+        astraAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        verticalPress = Input.GetAxis("Vertical");
+        astraAnimator.SetFloat("Blend", verticalPress);
+    }
+}
